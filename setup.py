61ad50ead92337ef50079dfe0a43ce355df7e774
Pierre Karashchuk <krchtchk@gmail.com>
2019-08-19 12:35:34 -0700

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="calligator",
-    version="0.2.4",
+    version="0.2.5",
     author="Pierre Karashchuk",
     author_email="krchtchk@gmail.com",
     description="An easy-to-use library for calibrating cameras in python",
