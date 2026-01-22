3085459a4fd2458f306d8c952eb34d8acb54af00
Lili Karashchuk <krchtchk@gmail.com>
2026-01-22 14:42:31 -0500

    switch to pyproject.toml

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-rm -rv dist build *.egg-info
-python3 setup.py sdist bdist_wheel
+rm -frv dist build *.egg-info
+# python3 setup.py sdist bdist_wheel
+python -m build
 twine upload dist/*
