88d0d393c2c38a7d71736213ad9dd54eebb543d5
Lili Karashchuk <krchtchk@gmail.com>
2026-04-22 12:05:00 -0400

    add mouse test calibration

--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -4,12 +4,30 @@ import pytest
 
 import numpy as np
 
+import importlib
+import aniposelib.cameras
+importlib.reload(aniposelib.cameras)
+
 from aniposelib.cameras import Camera, CameraGroup
 from aniposelib.boards import CharucoBoard, Checkerboard
 
 import os
 import toml
 import pickle
+
+import torch
+import time
+
+SEED = 42
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(SEED)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 # copied from anipose
 def get_calibration_board(config):
@@ -55,6 +73,7 @@ def test_7cam_allen_calibration():
     for cam, cref in zip(cgroup.cameras, cgroup_ref.cameras):
         cam.set_size(cref.get_size())
 
+    cgroup = cgroup.to(device)
 
     with open(detection_path, 'rb') as f:
         all_rows = pickle.load(f)
@@ -63,7 +82,10 @@ def test_7cam_allen_calibration():
     board = get_calibration_board(config)
 
     err = cgroup.calibrate_rows(all_rows, board)
-    assert err < 1
+    # print(err)
+    assert err < 1.5
+
+    return err
 
 @pytest.mark.integration
 def test_12cam_allen_calibration():
@@ -78,6 +100,7 @@ def test_12cam_allen_calibration():
     for cam, cref in zip(cgroup.cameras, cgroup_ref.cameras):
         cam.set_size(cref.get_size())
 
+    cgroup = cgroup.to(device)
 
     with open(detection_path, 'rb') as f:
         all_rows = pickle.load(f)
@@ -86,4 +109,41 @@ def test_12cam_allen_calibration():
     board = get_calibration_board(config)
 
     err = cgroup.calibrate_rows(all_rows, board)
-    assert err < 1
+    # print(err)
+    assert err < 1.5
+
+    return err
+
+
+def see_mouse_outdoor_4_10():
+    prefix = 'tests/data/calib/mouse-outdoor'
+    calib_path = os.path.join(prefix, 'calibration_right_4_10_2026_adjusted.toml')
+    point_path = os.path.join(prefix, 'points_right_4_10_2026_sub.npz')
+
+    cgroup = CameraGroup.load(calib_path).to(device)
+    dd = np.load(point_path)
+
+    p2d_sub = dd['p2d']
+
+    cgroup.bundle_adjust_iter(p2d_sub, verbose=True)
+
+    p3d = cgroup.triangulate(p2d_sub, progress=False)
+    err = cgroup.reprojection_error(p3d, p2d_sub, mean=True)
+    outerr = torch.median(err).item()
+
+    # print(outerr)
+    assert outerr < 10
+
+    return outerr
+
+# start_time = time.time()
+
+# print(test_7cam_allen_calibration())
+# print(test_12cam_allen_calibration())
+# print(see_mouse_outdoor_4_10())
+
+# end_time = time.time()
+
+# total_time = end_time - start_time
+
+# print("total time: ", total_time)
